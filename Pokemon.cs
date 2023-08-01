9aaa6a96dfd59d88c5d95ef2b552a3fae9b790b9
Randall Smith <rdsmith1761@gmail.com>
2023-07-31 20:15:22 -0400

    Initial Push

---
+++ Pokemon.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections;
+
+namespace Final_Project_Console_App
+{
+    public class Pokemon
+    {
+        public static void  PokemonCard(string cardName)
+        {
+            var client = new RestClient($"https://pokemon-tcg-card-prices.p.rapidapi.com/card?name={cardName}");
+
+            var request = new RestRequest();
+
+            request.AddHeader("X-RapidAPI-Key", "ff84c857bbmsheb2bb98fa878952p191cb2jsna8a78f2f9855");
+
+            request.AddHeader("X-RapidAPI-Host", "pokemon-tcg-card-prices.p.rapidapi.com");
+
+            var response = client.Execute(request).Content;
+
+            Console.WriteLine(response);
+
+            var newCardInfo = JsonConvert.DeserializeObject<CardInfo>(response);
+
+
+            Console.WriteLine($"{newCardInfo}");
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Final_Project_Console_App;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+
+
+//Console.WriteLine("Please enter currency type: GBP or UDS");
+//var currency = Console.ReadLine();
+
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please enter your card name");
+        var cardName = Console.ReadLine();
+
+
+        Pokemon.PokemonCard(cardName);
+        
+    }
+}
